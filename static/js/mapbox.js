ce002d598ae15406fd125177a39dcf852286044c
zach steele <zacharysteele8@gmail.com>
2020-03-20 22:29:17 -0500

    updating 3/20/2020

--- static/js/mapbox.js
+++ static/js/mapbox.js
@@ -2,75 +2,100 @@
 API_KEY = 'pk.eyJ1IjoiemRzdGVlbGUiLCJhIjoiY2sycnJsejJyMGJsajNtbnhqNWdhMXk3bCJ9.iDhD7eDSPOc1WErl0o8riQ'
 
 $.getJSON("/static/js/map.json", function (data) {
-    console.log(data);
 
-    createFeatures(data.features)
+  // Function to determine marker size based on population
+  function markerSize(cases) {
+    return (cases * 3000);
+  }
 
-  
-0
+  function onEachFeature(feature, layer) {
+    // does this feature have a property named popupContent?
+    layer.bindPopup("<h4>" + feature.properties.Texas_County +
+      "</h3><hr><p>" + feature.properties.COVID_19_Cases + "</p>");
+
+  }
+
+  var locations = data.features
+
+
+  console.log(locations)
+
+  // Define arrays to hold created city and state markers
+  // var cityMarkers = [];
+  var countyMarkers = [];
+
+  // Loop through locations and create city and state markers
+  for (var i = 0; i < locations.length; i++) {
+    // Setting the marker radius for the state by passing population into the markerSize function
+
+    countyMarkers.push(
+      L.circle(locations[i].geometry.coordinates, {
+        stroke: false,
+        fillOpacity: 0.75,
+        color: "white",
+        fillColor: "red",
+        radius: markerSize(locations[i].properties.COVID_19_Cases)
+      }).bindPopup("<h4>" + locations[i].properties.Texas_County +
+        "</h3><hr><p>" + locations[i].properties.COVID_19_Cases + "</p>")
+
+    );
+
+    // Setting the marker radius for the city by passing population into the markerSize function
+    // cityMarkers.push(
+    //   L.circle(locations[i].coordinates, {
+    //     stroke: false,
+    //     fillOpacity: 0.75,
+    //     color: "purple",
+    //     fillColor: "purple",
+    //     radius: markerSize(locations[i].city.population)
+    //   })
+    // );
+  }
+
+  // Define variables for our base layers
+  var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+  });
+
+  var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.dark",
+    accessToken: API_KEY
+  });
+
+  // Create two separate layer groups: one for cities and one for states
+  var texas = L.layerGroup(countyMarkers);
+  // var cities = L.layerGroup(cityMarkers);
+
+  // Create a baseMaps object
+  var baseMaps = {
+    "Street Map": streetmap,
+    "Dark Map": darkmap
+  };
+
+  // Create an overlay object
+  var overlayMaps = {
+    "Covid-19-Cases": texas
+    // "City Population": cities
+  };
+
+  // Define a map object
+  var myMap = L.map("map", {
+    center: [29.00, -99.90],
+    zoom: 6,
+    layers: [streetmap, texas]
+  });
+
+  // Pass our map layers into our layer control
+  // Add the layer control to the map
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+
+
+
 });
-
-function createFeatures(earthquakeData) {
-
-    // Define a function we want to run once for each feature in the features array
-    // Give each feature a popup describing the place and time of the earthquake
-    function onEachFeature(feature, layer) {
-      layer.bindPopup("<h3>" + feature.properties.Texas_County +
-        "</h3><hr><p>" + feature.properties.COVID_19_Cases+ "</p>");
-    }
-  
-    // Create a GeoJSON layer containing the features array on the earthquakeData object
-    // Run the onEachFeature function once for each piece of data in the array
-    var earthquakes = L.geoJSON(earthquakeData, {
-      onEachFeature: onEachFeature
-    });
-  
-    // Sending our earthquakes layer to the createMap function
-    createMap(earthquakes);
-  }
-  
-  function createMap(earthquakes) {
-  
-    // Define streetmap and darkmap layers
-    var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-      maxZoom: 18,
-      id: "mapbox.streets",
-      accessToken: API_KEY
-    });
-  
-    var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-      maxZoom: 18,
-      id: "mapbox.dark",
-      accessToken: API_KEY
-    });
-  
-    // Define a baseMaps object to hold our base layers
-    var baseMaps = {
-      "Street Map": streetmap,
-      "Dark Map": darkmap
-    };
-  
-    // Create overlay object to hold our overlay layer
-    var overlayMaps = {
-      Earthquakes: earthquakes
-    };
-  
-    // Create our map, giving it the streetmap and earthquakes layers to display on load
-    var myMap = L.map("map", {
-      center: [
-        31.96, -99.90
-      ],
-      zoom: 5,
-      layers: [streetmap, earthquakes]
-    });
-  
-    // Create a layer control
-    // Pass in our baseMaps and overlayMaps
-    // Add the layer control to the map
-    L.control.layers(baseMaps, overlayMaps, {
-      collapsed: false
-    }).addTo(myMap);
-  }
-  
