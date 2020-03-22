195886cda3f51efffa00d0aee673814d087df76c
zach steele <zacharysteele8@gmail.com>
2020-03-21 19:06:17 -0500

    pushing table

--- static/js/Harris.js
+++ static/js/Harris.js
@@ -1,26 +1,46 @@
-d3.csv("/static/csv/harris_stats.csv").then(function (harris_stats) {
-
-
-
-    // console.log(harris_stats);
-
+d3.csv("/static/csv/harris_stats.csv").then(function(harris_stats) {
+    // Example found from here
+    // http://bl.ocks.org/yan2014/c9dd6919658991d33b87
     var harrisArray = [];
 
     harris_stats.forEach(function(row, i) {
 
-        // console.log(d)
-        console.log(row)
+        harrisArray.push([row.Sex, row.Location, row.Age_Range, row.Status])
+    });
 
+    var table = d3.select("#table")
+    var header = table.append("thead").append("tr");
+    header
+        .selectAll("th")
+        .data(["Sex", "Location", "Age_Range", "Status"])
+        .enter()
+        .append("th")
+        .text(function(d) { return d; });
 
-        
+    var tablebody = table.append("tbody");
+    rows = tablebody
+        .selectAll("tr")
+        .data(harrisArray)
+        .enter()
+        .append("tr");
+
+    cells = rows.selectAll("td")
+        .data(function(d) {
+            return d;
+        })
+        .enter()
+        .append("td")
+        .text(function(d) {
+            return d;
+        });
+    $(document).ready(function() {
+        $('#table').DataTable({
+            "scrollY": "50vh",
+            "scrollCollapse": true,
+            paging: false,
+        });
+        $('.dataTables_length').addClass('bs-select');
     });
 
 
-
-
-
-
-
-
-
-})
+});
