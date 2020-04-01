7c903da20844c9423d9493724dab7ae2aa30517f
zach steele <zacharysteele8@gmail.com>
2020-03-31 23:28:24 -0500

    pushing

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from splinter import Browser
 from selenium.webdriver.common.keys import Keys
 import time
 import json
+import requests
 from flask import Flask, render_template, jsonify, request, redirect
 
 
@@ -25,6 +26,42 @@ def Harris():
 @app.route("/Memes")
 def Memes():
     return render_template("Memes.html")
+
+@app.route("/Scrape")
+def Scrape():
+
+    url = 'https://www.reddit.com/r/CoronavirusMemes/'
+
+    # Path to chromedriver.exe
+    executable_path = {'executable_path':'chromedriver/chromedriver.exe'}
+
+    # Open the Splinter Browswer
+    browser = Browser('chrome', **executable_path, headless=True)
+
+    browser.visit(url)
+    time.sleep(10)
+
+    html = browser.html
+    soup = bs(html, 'html.parser')
+
+    memes = soup.find_all('div', class_='_3Oa0THmZ3f5iZXAQ0hBJ0k')
+
+    images = []
+
+    for i in memes:
+    
+        image = i.find('img')['src']
+        images.append(image)
+
+    meme_df = pd.DataFrame({"Memes":images})
+    meme_df.to_csv('static/csv/memes.csv', index=False, header=True)
+
+    browser.quit()
+
+    return jsonify(meme_df.to_dict('records'))
+    
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/Memes.js
+++ static/js/Memes.js
@@ -1,23 +1,49 @@
-d3.csv("/static/csv/memes.csv").then(function(Meme_data) {
+// d3.csv("/static/csv/memes.csv").then(function(Meme_data) {
 
 
-    console.log(Meme_data)
+//     console.log(Meme_data)
 
-    var images = d3.select('#meme_place').selectAll('div')
-        .data(Meme_data)
-        .enter()
-        .append('div')
-        .style('margin-bottom', '20px')
-        .style('border', '5px solid black')
-        .style('width', 'fit-content')
-        .html(function(d) {
+//     var images = d3.select('#meme_place').selectAll('div')
+//         .data(Meme_data)
+//         .enter()
+//         .append('div')
+//         .style('margin-bottom', '20px')
+//         .style('border', '5px solid black')
+//         .style('width', 'fit-content')
+//         .html(function(d) {
 
-            return "<img" + " " + "src=" + String(d.Memes) + ">"
+//             return "<img" + " " + "src=" + String(d.Memes) + ">"
 
-        })
+//         })
+
+// });
 
 
+function Go_Scrape() {
+    d3.json('/Scrape').then(function(meme_object) {
 
 
+        var images = d3.select('#meme_place').selectAll('div')
+            .data(meme_object)
+            .enter()
+            .append('div')
+            .style('margin-bottom', '20px')
+            .style('border', '5px solid black')
+            .style('width', 'fit-content')
+            .html(function(d) {
 
+                return "<img" + " " + "src=" + String(d.Memes) + ">"
+            })
+    });
+
+    var closer = function() {
+        document.getElementById("load").style.display = 'none'
+    };
+
+    closer();
+
+};
+
+d3.select('#scraper').on("click", () => {
+    d3.select('#load').style('display', "block")
 });
